05836c5a8404e03b0cbc08b5f8c76073db8867a0
xvrqt <git@xvrqt.com>
2025-11-09 13:04:43 -0800

    Removed individual wireguard configs and built a funtion which computes peers based off the machines list

--- wireguard/default.nix
+++ wireguard/default.nix
@@ -1,7 +1,5 @@
 { cfg, lib, name, config, ... }:
 let
-  # Wireguard interface name
-  interface = cfg.wireguard.interface;
   # Which point endpoints should keep open for connection
   port = 16842;
   # Persistent Keep Alive timing in seconds
@@ -11,44 +9,74 @@ let
   machines = cfg.machines;
   machine = machines."${name}";
 
-  # Decrypts & Deploys the WG Key
-  deploySecret = name: {
-    # The secret file that will be decrypted
-    file = ./secrets + ("/" + "${name}.wg.key");
-    # Folder to decrypt into (config.age.secretDir/'path')
-    name = "wg/private.key";
+  cfgCheck = config.networking.amy-net.enable;
 
-    # File Permissions
-    mode = "400";
-    owner = "root";
-
-    # Symlink from the secretDir to the 'path'
-    # Doesn't matter since both are in the same partition
-    symlink = true;
+  # Remove ourselves from our peers
+  machine_list = lib.attrsets.mapAttrsToList (name: value: value // { inherit name; }) machines;
+  peer_list = builtins.filter (machine: machine.name != name) machine_list;
+  # Function which creates a peer entry for each peer machine
+  create_peer_attrset = machine: {
+    name = machine.name;
+    publicKey = "${machine.wg.publicKey}";
+    endpoint = lib.mkIf (machine?endpoint) machine.ip.v4.www;
+    allowedIPs = [ "${machine.ip.v4.wg}/32" ];
+    persistentKeepalive = pka;
   };
+  # A list of peer attribute sets
+  peers = map create_peer_attrset peer_list;
 in
 {
-  # Decrypt & Deploy the WG Key
-  age.secrets.wgPrivateKey = deploySecret name;
+  options = {
+    networking = {
+      amy-net = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Create Wireguard interface 'amy-net' which connects all my machines.";
+        };
 
-  # If we are publicly available for other machines to connect to use, ensure
-  # the Wireguard UDP port is open
-  networking.firewall = lib.mkIf (machine.wg?endpoint) {
-    allowedUDPPorts = [ port ];
+      };
+    };
   };
 
-  # Setup the Wireguard Network Interface
-  networking.wireguard.interfaces = {
-    # Interface names are arbitrary
-    "${interface}" = {
-      # The machine's IP and the subnet (10.128.0.X/24) which the interface will capture and route traffic
-      ips = [ "${machine.ip.v4.wg}/24" ];
-      # Key that is used to encrypt traffic
-      privateKeyFile = config.age.secrets.wgPrivateKey.path;
-      # The port we're listening on if we're an endpoint
-      listenPort = lib.mkIf (machine.wg?endpoint) port;
-      # A list of peers to connect to, and allow connections to
-      peers = (import (./. + "/${name}.nix") { inherit pka port machines; });
+  config = lib.mkIf cfgCheck {
+    # Decrypt & Deploy the WG Key
+    age.secrets.wgPrivateKey = {
+
+      # The secret file that will be decrypted
+      file = ./secrets + ("/" + "${name}.wg.key");
+      # Folder to decrypt into (config.age.secretDir/'path')
+      name = "wg/private.key";
+
+      # File Permissions
+      mode = "400";
+      owner = "root";
+
+      # Symlink from the secretDir to the 'path'
+      # Doesn't matter since both are in the same partition
+      symlink = true;
+    };
+
+    # If we are publicly available for other machines to connect to use, ensure
+    # the Wireguard UDP port is open
+    networking.firewall = lib.mkIf (machine.wg?endpoint) {
+      allowedUDPPorts = [ port ];
+    };
+
+    # Setup the Wireguard Network Interface
+    networking.wireguard.interfaces = {
+      # Interface names are arbitrary
+      "${cfg.wireguard.interface}" = {
+        # The machine's IP and the subnet (10.128.0.X/24) which the interface will capture and route traffic
+        ips = [ "${machine.ip.v4.wg}/24" ];
+        # Key that is used to encrypt traffic
+        privateKeyFile = config.age.secrets.wgPrivateKey.path;
+        # The port we're listening on if we're an endpoint
+        listenPort = lib.mkIf (machine.wg?endpoint) port;
+        # A list of peers to connect to, and allow connections to
+        inherit peers;
+      };
     };
   };
 }
+
