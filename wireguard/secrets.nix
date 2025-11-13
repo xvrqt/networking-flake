d443b93290a6179d0a6199679b81673f2d1cf889
crow <git@xvrqt.net>
2025-11-13 14:12:46 -0800

    Added in the taven key and machine attrset

--- wireguard/secrets.nix
+++ wireguard/secrets.nix
@@ -12,4 +12,6 @@ in
   "secrets/nyaa.wg.key".publicKeys = [ publicKeys.machines.nyaa ];
   # The Lighthouse's Wireguard private key, only accessible to itself
   "secrets/lighthouse.wg.key".publicKeys = [ publicKeys.machines.lighthouse ];
+  # The Tavern's Wireguard private key, only accessible to itself
+  "secrets/tavern.wg.key".publicKeys = [ publicKeys.machines.tavern ];
 }
