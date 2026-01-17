76149ce305e0bb46579a6bf0b7b75a8d914e960d
crow <git@xvrqt.com>
2026-01-17 13:05:00 -0800

    Update headscale/default.nix

--- headscale/default.nix
+++ headscale/default.nix
@@ -43,7 +43,7 @@ in
 
         settings = {
           # Where new clients can sign up
-          server_url = "http://${gateway_subdomain}";
+          server_url = "https://${gateway_subdomain}";
 
           # This is what the developers recommend and what they test on
           # PostGres seems heavy for such a simple VPS 
